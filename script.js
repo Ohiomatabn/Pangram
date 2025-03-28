1d3e0d0acbccca486bdd02e1698414aeb721790b
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-03-28 16:10:38 +0100

    Rectify some typing errors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function checkPangram(){
-  const senctence = document.getElementById('pangram').value;
+  const sentence = document.getElementById('pangram').value;
   const result = document.getElementById('result');
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   for (let char of alphabet){
-    if (!senctence.toLowerCase().includes(char)){
-      result.innerHTML = 'These senctence is not a Pangram, <br> a Pangram must contain all the senctence of alphabet';
+    if (!sentence.toLowerCase().includes(char)){
+      result.innerHTML = `${sentence.toUpperCase()} is not a Pangram`;
     } else{
-      result.innerHTML = "It's a pangram, <br> it contain all the letter of the alphabet";
+      result.innerHTML = `${sentence.toUpperCase()} is a Pangram`;
     }
   }
 }
